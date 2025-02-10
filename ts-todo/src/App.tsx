54d2cbcdeabfac941754ddeb8168f1f05dee5994
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-02-10 21:26:33 +0530

    added some stuff

--- ts-todo/src/App.tsx
+++ ts-todo/src/App.tsx
@@ -1,10 +1,50 @@
 import { Navigate } from 'react-router-dom'
-import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import NewNote from './components/NewNote'
+import { useLocalStorage } from './hooks/useLocalStorage'
+import { useMemo } from 'react'
+
+export type Note = {
+  id: string,
+} & NoteData //this gives all the properties of notedata to note and also adds id to it
+
+export type NoteData = {
+  title: string,
+  markdown: string,
+  tags: Tag[];
+} 
+
+export type Tag = {
+  id: string,
+  label: string
+}
+
+export type RawNote = {
+  id: string,
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string,
+  markdow: string,
+  tagIds: string[]
+}
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
+  const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', []);
+  
+  const noteswithTags = useMemo(() =>{
+    return notes.map(note =>{
+      return {...note, tags:tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes, tags])
+
+  function onCreateNote({tags, ...data}: NoteData){
+    setNotes(prevNotes => {
+      return [...prevNotes, {...data, id: uuidV4()}]
+    })
+  }
   return (
     <Container className='my-4'>
     <Routes>
--- ts-todo/src/components/NoteForm.tsx
+++ ts-todo/src/components/NoteForm.tsx
@@ -1,25 +1,68 @@
-import { Col, Form, Row, Stack } from 'react-bootstrap'
+import { FormEvent, useRef, useState } from 'react'
+import { Button, Col, Form, Row, Stack } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import CreatableSelect from 'react-select/creatable'
+import { NoteData, Tag} from '../App'
 
-function NoteForm() {
-  return <Form>
+type NoteFormProps = {
+    onSubmit:(data:NoteData) => void
+}
+
+function NoteForm({onSubmit}:NoteFormProps) {
+
+    const titleRef = useRef<HTMLInputElement>(null);
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  
+    function handleSubmit(e:FormEvent){
+        e.preventDefault();
+
+        onSubmit({
+            title: titleRef.current!.value,
+            markdown: textAreaRef.current!.value,
+            tags: [],
+        })
+    }
+
+  return (
+  <Form onSubmit={handleSubmit}>
     <Stack gap={4}>
         <Row>
             <Col>
                 <Form.Group controlId = "title">
                     <Form.Label>Title</Form.Label>
-                    <Form.Control required />
+                    <Form.Control ref ={titleRef} required />
                 </ Form.Group>
             </Col>
             <Col>
                 <Form.Group controlId="tags">
                     <Form.Label>Tags</Form.Label>
-                    <CreatableSelect isMulti />
+                    <CreatableSelect value={selectedTags.map(tag => {
+                        return {label: tag.label, value: tag.id}
+                    })}
+                    onChange={tags => {
+                        setSelectedTags(tags.map(tag=>{
+                            return {label: tag.label, id: tag.value}
+                        }))
+                    }} 
+                    isMulti />
+            
                 </Form.Group>
             </Col>
         </Row>
+        <Form.Group controlId="markdown">
+            <Form.Label>Body</Form.Label>
+            <Form.Control required as="textarea" rows = {15} ref={textAreaRef}/>
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+            <Button type="submit" variant="primary">Save</Button>
+            <Link to="..">
+                <Button type="button" variant="outline-secondary">Cancel</Button>
+            </Link>
+        </Stack>
     </Stack>
   </Form>
+  )
 }
 
 export default NoteForm
