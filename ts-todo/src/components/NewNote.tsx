a53d55c65885864e20d1db778c2c704d5eecda08
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-02-10 19:02:27 +0530

    added some stuff

---
+++ ts-todo/src/components/NewNote.tsx
@@ -0,0 +1,11 @@
+import NoteForm from './NoteForm'
+function NewNote() {
+  return (
+    <div>
+        <h1 className='mb-4'>New Note</h1>
+        <NoteForm />
+    </div>
+  )
+}
+
+export default NewNote
