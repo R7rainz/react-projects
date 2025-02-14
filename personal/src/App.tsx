6fcfaff9c51133d40ed01506172cff4f7de7e784
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-02-14 19:42:26 +0530

    added some components and functionalities

--- personal/src/App.tsx
+++ personal/src/App.tsx
@@ -1,6 +1,12 @@
 import './App.css'
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import Header from './components/Header'
+import Home from './components/Home'
+import Services from './components/Services'
+import About from './components/About'
+import Contact from './components/Contact'
+import Login from './components/Login'
+import GetStarted from './components/Getstarted'
 
 function App() {
   return (
@@ -9,6 +15,12 @@ function App() {
         <Header />
         <main className='flex-grow'>
           <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/get-started" element={<GetStarted />} />
           </Routes>
         </main>
       </div>
