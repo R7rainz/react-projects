3c60924be85d2afdb0226ba29116b2c8718af52d
Ronak Kamboj <ronakkamboj26@gmail.com>
2025-03-16 00:59:56 +0530

    added latest tailwind v4 and shadcn stuff

--- codequest-lite/src/App.tsx
+++ codequest-lite/src/App.tsx
@@ -1,7 +1,12 @@
-export default function App() {
+import { Button } from "@/components/ui/button"
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Button>Click me</Button>
+      <h1 className="text-red-400">hi there</h1>
+    </div>
   )
 }
+
+export default App
